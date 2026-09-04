6c352df3101bcf323216010eb14b1236fb80e4a1
duncankn2-coder <duncankn2@gmail.com>
2026-09-04 14:35:10 +0800

    feat: initialize root layout with global styles and header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,20 +2,17 @@
 import Header from './components/Header';
 import './globals.css';
 import { ReactNode } from 'react';
-import { getSiteContext } from './utils/siteContext';
 
 export const metadata = {
   title: 'Megaman',
   description: 'The new Megaman Official website',
 };
 
-export default async function RootLayout({ children }: { children: ReactNode }) {
-  const siteContext = await getSiteContext();
-
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <Header initialSiteContext={siteContext} />
+        <Header />
         <main>{children}</main>
       </body>
     </html>
